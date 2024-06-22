6e052f574aa39aaa9809af718ac8cb3aea6162d3
Gema <144107137+gema17@users.noreply.github.com>
2024-06-22 16:47:11 +0700

    Delete Jobsheet 1 directory

--- Jobsheet 1/Introduction.cpp
+++
@@ -1,8 +0,0 @@
-//Created by 23343066_Gema Pratama
-
-#include <stdio.h>
-int main()
-{
-	printf("\nWelcome to the world of C"); //prints the message on the screen
-	return 0; //returns a value 0 to the operating system
-}
